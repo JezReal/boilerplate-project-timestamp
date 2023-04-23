dd348284e532c5a74dfe691ad86c1567fdd6d334
Jezreel <jezreeljoshuam@gmail.com>
2023-04-23 22:53:01 +0800

    configure for vercel deployment

--- api/index.js
+++ api/index.js
@@ -3,26 +3,31 @@
 var dotenv = require("dotenv").config();
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api", (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  response.end();
+});
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
 app.get("/api/:date", (request, response) => {
@@ -37,9 +42,9 @@ app.get("/api/:date", (request, response) => {
   const unixTime = Math.floor(utcDate.getTime() / 1000);
 
   response.json({ unix: unixTime, utc: utcDate.toUTCString() });
-})
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
